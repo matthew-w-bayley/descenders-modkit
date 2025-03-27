3a9e10d59cec5995af8c041bc782157a7507853f
Matthew Bayley <nohumanman@gmail.com>
2025-03-27 21:27:40 +0000

    🚑️ Update replay upload check to require minimum byte length

--- mod-loader/mod-loader-solution/src/NetClient.cs
+++ mod-loader/mod-loader-solution/src/NetClient.cs
@@ -187,7 +187,7 @@ namespace ModLoaderSolution
 		public void UploadReplay(string replay, string time_id)
 		{
 			Byte[] bytes = System.IO.File.ReadAllBytes(replay);
-			if (bytes.Length == 0)
+			if (bytes.Length < 10)
 			{
 				Utilities.instance.PopUp("Replay failed to upload!", "Time will be invalidated...");
 				return;
